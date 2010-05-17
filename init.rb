743508ece55f1d7d62739db109236373d62c7355
Douglas Campos <douglas@theros.info>
2010-05-17 08:33:01 -0300

    trying to fixup bundler

--- init.rb
+++ init.rb
@@ -1,5 +1,13 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
-require ".bundle/environment"
-require "sinatra"
+require "rubygems"
+require "bundler"
+Bundler.setup
+
+require "sinatra/base"
 require "haml"
 require "qblog"
+
+require 'active_record'
+
+dbconfig = YAML.load(File.read('config/database.yml'))
+ActiveRecord::Base.establish_connection dbconfig['production']
